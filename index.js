1316e43045fee983d87b5cc6a3ebdffdf3370211
danilo silva <ds5695749@gmail.com>
2024-01-10 09:58:33 -0300

    ajusta layout da landing page
    -ADD botão voltar para o topo
    -ADD ancora no menu
    -ADD listagem em ordem alfabética do time

---
+++ index.js
@@ -0,0 +1,41 @@
+$(document).ready(function () {
+    'use strict';
+
+    const $buttonProjeto = $(".btn_projeto")
+    const $buttonSolucao = $(".btn_solucao")
+    const $buttonProblema = $(".btn_problema")
+    const $buttonNoticias = $(".btn_noticias")
+    const $buttonTime = $(".btn_time")
+    const $buttonContato = $(".btn_contato")
+    
+    const $secaoProjeto = $("#about")
+    const $secaoProblema = $("#projects")
+    const $secaoSolucao = $("#services")
+    const $secaoNoticias = $("#latest-news")
+    const $secaoTime = $("#team")
+    const $secaoContatos = $("#contact-us")
+    const $secaoMenu = $(".top-header")
+    const $btnSubir = $(".btn_fixed")
+
+    $buttonProjeto.on('click', irPara.bind({}, $secaoProjeto))
+    $buttonSolucao.on('click', irPara.bind({}, $secaoSolucao))
+    $buttonProblema.on('click',irPara.bind({}, $secaoProblema))
+    
+    $buttonNoticias.on('click',irPara.bind({}, $secaoNoticias))
+    $buttonTime.on('click',irPara.bind({}, $secaoTime))
+    $buttonContato.on('click',irPara.bind({}, $secaoTime))
+    $btnSubir.on('click',irPara.bind({}, $secaoMenu))
+
+    $(window).on('scroll', function () {
+        if (window.scrollY == 0) {
+            $btnSubir.removeClass("visible")
+        } else {
+            $btnSubir.addClass("visible")
+        }
+    })
+
+    function irPara($secao) {
+        $("html, body").animate({ scrollTop: $secao.offset().top}, 1000);
+    }
+    
+});
